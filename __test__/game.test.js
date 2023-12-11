ee4dc31435cf2b4ce146f24b698cbaceed49daa7
Jack Scammell <30805808+JackScammell@users.noreply.github.com>
2023-12-11 10:18:38 +0000

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -2,8 +2,12 @@ const Game = require('../src/game').default
 const fs = require('fs')
 
 describe('App', () => {
-  it('Contains the compiled JavaScript', async () => {
-  
+  test.skip('Contains the compiled JavaScript', async (done) => {
+    fs.readFile('./public/main.js', 'utf8', (err, data) => {
+      expect(err).toBe(null)
+      expect(data).toMatchSnapshot()
+      done()
+    })
   })
 })
 
